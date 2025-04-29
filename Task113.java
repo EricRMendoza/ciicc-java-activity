3f032e1297641f951283e273e0b4885cfff65bae
E MENDOZA <mendozaeric2070@gmail.com>
2025-04-29 10:19:01 +0800

    Update Task113.java
    Signed-off-by: E MENDOZA <mendozaeric2070@gmail.com>

--- Task113.java
+++ Task113.java
@@ -1,1 +1,10 @@
-
+ublic class Task113 {
+    public static void main(String[] args) {
+        int check_number = 10;
+        
+        for (int i = 1; i <= check_number; i++) {
+            String message = (i % 2 == 0) ? "It is an even number" : "It is an odd number";
+            System.out.println(i + ": " + message);
+        }
+    }
+}
