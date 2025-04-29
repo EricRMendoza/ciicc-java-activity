45f3db1db7fdb47a0507061ba1ce3713abda0229
E MENDOZA <mendozaeric2070@gmail.com>
2025-04-29 09:33:49 +0800

    Add files via upload
    Signed-off-by: E MENDOZA <mendozaeric2070@gmail.com>

---
+++ Task111.java
@@ -0,0 +1,8 @@
+ public class Task111{/** 
+    * The Task 1-1-1 class implements an application that
+    * simply prints "Kamusta Mundo!" to standard output.
+    */
+       public static void main(String[] args){
+           System.out.println("Kamusta Mundo!");// Display the string 
+     }
+   }
