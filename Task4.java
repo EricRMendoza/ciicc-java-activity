c8c8cb5c6d6d11c95aa43797fc26843b0c3fa035
E MENDOZA <mendozaeric2070@gmail.com>
2025-04-30 16:56:45 +0800

    Create Task4.java
    Signed-off-by: E MENDOZA <mendozaeric2070@gmail.com>

---
+++ Task4.java
@@ -0,0 +1,18 @@
+public class Task4 {
+    public static void main(String[] args) {
+
+        StringBuilder str = new StringBuilder("love");
+        System.out.println("String = " + str.toString());
+          
+        String original = str.toString();
+           
+        str.reverse();
+        System.out.println("Reverse String = " + str.toString());
+          
+        if (original.equals(str.toString())) {
+            System.out.println("The input string is a palindrome");
+        } else {
+            System.out.println("The input string is not a palindrome");
+        }
+    }
+}
