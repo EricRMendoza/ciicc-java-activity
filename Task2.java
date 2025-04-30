08ee8803ce37714b18dc06ed56cb04600a6eaa3c
E MENDOZA <mendozaeric2070@gmail.com>
2025-04-30 15:43:07 +0800

    Create Task2.java
    Signed-off-by: E MENDOZA <mendozaeric2070@gmail.com>

---
+++ Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+    public static void main(String[] args) {
+        byte zero = 0;
+        String output ="H311" + zero + " W" + zero + "rld "+ "2.0 true";
+    System.out.println(output);
+        //
+  
+}
+}
