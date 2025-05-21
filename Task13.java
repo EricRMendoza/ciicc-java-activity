eb8aa63d0975616c07cf10c10767212edb2a9caf
E MENDOZA <mendozaeric2070@gmail.com>
2025-05-21 12:53:51 +0800

    Update Task13.java
    Signed-off-by: E MENDOZA <mendozaeric2070@gmail.com>

--- Task13.java
+++ Task13.java
@@ -1,12 +1,10 @@
-// Task13.java
 
-// Base class Vehicle
 class Vehicle {
     String make;
     String model;
     int year;
 
-    // Constructor for Vehicle
+   
     public Vehicle(String make, String model, int year) {
         this.make = make;
         this.model = model;
@@ -14,17 +12,17 @@ class Vehicle {
     }
 }
 
-// Derived class Car that extends Vehicle
+
 class Car extends Vehicle {
     int numberOfDoors;
 
-    // Constructor for Car
+  
     public Car(String make, String model, int year, int numberOfDoors) {
-        super(make, model, year); // Call to parent class constructor
+        super(make, model, year); 
         this.numberOfDoors = numberOfDoors;
     }
 
-    // Method to display details
+  
     public void displayDetails() {
         System.out.println("Car Details:");
         System.out.println("Make: " + make);
@@ -34,13 +32,13 @@ class Car extends Vehicle {
     }
 }
 
-// Main class to test the Car class
+
 public class Task13 {
     public static void main(String[] args) {
-        // Create an instance of Car
+    
         Car myCar = new Car("Toyota", "Camry", 2022, 4);
 
-        // Display car details
+     
         myCar.displayDetails();
     }
 }
